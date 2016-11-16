8e9914537b88a995671e7d3624d919aa7d045903
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2016-11-16 17:57:24 +0100

    Add connect_from_file to package root

--- appnexus/__init__.py
+++ appnexus/__init__.py
@@ -2,4 +2,4 @@ from .client import AppNexusClient, client, connect, find
 from .model import *
 
 __all__ = ["AppNexusClient", "Model", "client", "connect", "find",
-           "services_list"] + services_list
+           "services_list", "connect_from_file"] + services_list
