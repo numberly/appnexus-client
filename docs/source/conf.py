c1109c2e68310c44c3e65954d23faf7a878d9b8f
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2018-03-30 08:37:01 +0000

    Setup documentation build

---
+++ docs/source/conf.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'AppNexus-client'
+copyright = '2018, numberly'
+author = 'numberly'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+# The master toctree document.
+master_doc = 'index'
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. AppNexus-client documentation master file, created by
+   sphinx-quickstart on Fri Mar 30 08:34:05 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to AppNexus-client's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
